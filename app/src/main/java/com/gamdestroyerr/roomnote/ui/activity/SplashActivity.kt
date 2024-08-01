43f1e5444fd10157d4edea98b30a596b5ebedf4e
zayn <cristiancitotor12345@gmail.com>
2024-08-01 07:54:33 -0500

    intentandolo

---
+++ app/src/main/java/com/gamdestroyerr/roomnote/ui/activity/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.gamdestroyerr.roomnote.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Establecer el retraso en milisegundos
+        val splashScreenDelay: Long = 3000
+
+        // Usar Handler para esperar el tiempo especificado antes de iniciar NoteActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Iniciar NoteActivity después del retraso
+            val intent = Intent(this@SplashActivity, NoteActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashScreenDelay)
+    }
+}
